22ce29231da6e6bcf5043c300c0acc3ab034e9a5
singhbraj <braj@imoneypay.in>
2021-12-02 16:28:10 +0530

    sallow comparison added

---
+++ shallowCopy.js
@@ -0,0 +1,18 @@
+
+
+const user = {
+    name: "John",
+    surname: "Doe",
+    age:{
+      Age:23
+    }
+  }
+
+//   const user1 = user;
+
+  const user1 = {...user}
+  user1.age.Age=24
+  console.log(user)
+  console.log(user1)
+
+  console.log(user === user1)
