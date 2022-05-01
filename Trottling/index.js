48fd31918770371c283fed16b12ada1ea26f6eb2
singhbraj <braj@imoneypay.in>
2022-05-01 21:35:32 +0530

    trottling is added

---
+++ Trottling/index.js
@@ -0,0 +1,43 @@
+
+const loggerFunc = () =>{
+
+    console.log("Throttled Function")
+}
+
+
+const throttle = (fn,limit) =>{
+
+    let flag = true
+        return function(){
+
+            let context = this;
+            // console.log(context)
+            let args = arguments;
+        
+            if(flag){
+                fn.apply(context,args)
+                flag=false;
+                setTimeout(()=>{
+                   flag =true
+                },limit)
+            }
+
+        }
+
+} 
+
+
+const betterLoggerFunction = throttle(loggerFunc,1000)
+
+
+
+
+window.addEventListener("resize",betterLoggerFunction)
+
+
+// const normalFunc = () =>{
+//     console.count("Normal Function")
+// }
+
+// window.addEventListener("resize", normalFunc)
+
